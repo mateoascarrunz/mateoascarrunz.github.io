3c76b72c5ca88ba173e58c8a38a9ace66a38be3e
mateoascarrunz <87576790+mateoascarrunz@users.noreply.github.com>
2023-01-15 16:29:32 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -50,5 +50,32 @@ const hiddenElements = document.querySelectorAll (".hidden, .hiddenleft, .hidden
 hiddenElements.forEach((el) => observer.observe(el));
 
 
-  
+// scroll with the mouse 
+const slider = document.querySelector('.media-scroller');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1.2; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
   
