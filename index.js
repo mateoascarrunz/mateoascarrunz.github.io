fe50c9d76f7af264e30f6ceb27a8134dd4d63314
mateoascarrunz <87576790+mateoascarrunz@users.noreply.github.com>
2023-04-25 10:51:34 +0200

    Improve performance

--- index.js
+++ index.js
@@ -1,81 +1,76 @@
 // SVG scroll drawing
-let path = document.querySelector("path")
-let pathLength = path.getTotalLength()
+let path = document.querySelector("path");
+let pathLength = path.getTotalLength();
 
 path.style.strokeDasharray = pathLength + " " + pathLength;
 
-path.style.strokeDashoffset =pathLength;
+path.style.strokeDashoffset = pathLength;
 
 window.addEventListener("scroll", () => {
-    
-    var scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-    var drawLength = pathLength * scrollPercentage;
-    path.style.strokeDashoffset = pathLength - drawLength;
-})
+  var scrollPercentage =
+    (document.documentElement.scrollTop + document.body.scrollTop) /
+    (document.documentElement.scrollHeight -
+      document.documentElement.clientHeight);
+  var drawLength = pathLength * scrollPercentage;
+  path.style.strokeDashoffset = pathLength - drawLength;
+});
 
 // SVG out
-$(window).scroll(function(){
-    var numPix = 3000; // number of pixels before bottom of page that you want to start fading
-    var op = (($(document).height() - $(window).height()) - $(window).scrollTop()) / numPix;
-    var win = $(window).width();  
-    if( op <= 0 && win < 697){
-          $(".line-container").hide();
-      } else {
-          $(".line-container").show();
-      }
-      $(".line-container").css("opacity", op ); 
-  });
-
-
-
-
+$(window).scroll(function () {
+  var numPix = 3000; // number of pixels before bottom of page that you want to start fading
+  var op =
+    ($(document).height() - $(window).height() - $(window).scrollTop()) /
+    numPix;
+  var win = $(window).width();
+  if (op <= 0 && win < 697) {
+    $(".line-container").hide();
+  } else {
+    $(".line-container").show();
+  }
+  $(".line-container").css("opacity", op);
+});
 
 // entry Animation
 const observer = new IntersectionObserver((entries) => {
-
-
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show2");
-
-        } else{
-            entry.target.classList.remove("show2");
-        }
-    });
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show2");
+    } else {
+      entry.target.classList.remove("show2");
+    }
+  });
 });
 
-
-const hiddenElements = document.querySelectorAll (".hidden, .hiddenleft, .hiddenright, .hidden2");
+const hiddenElements = document.querySelectorAll(
+  ".hidden, .hiddenleft, .hiddenright, .hidden2"
+);
 hiddenElements.forEach((el) => observer.observe(el));
 
-
-// scroll with the mouse 
-const slider = document.querySelector('.media-scroller');
+// scroll with the mouse
+const slider = document.querySelector(".media-scroller");
 let isDown = false;
 let startX;
 let scrollLeft;
 
-slider.addEventListener('mousedown', (e) => {
+slider.addEventListener("mousedown", (e) => {
   isDown = true;
-  slider.classList.add('active');
+  slider.classList.add("active");
   startX = e.pageX - slider.offsetLeft;
   scrollLeft = slider.scrollLeft;
 });
-slider.addEventListener('mouseleave', () => {
+slider.addEventListener("mouseleave", () => {
   isDown = false;
-  slider.classList.remove('active');
+  slider.classList.remove("active");
 });
-slider.addEventListener('mouseup', () => {
+slider.addEventListener("mouseup", () => {
   isDown = false;
-  slider.classList.remove('active');
+  slider.classList.remove("active");
 });
-slider.addEventListener('mousemove', (e) => {
-  if(!isDown) return;
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
   e.preventDefault();
   const x = e.pageX - slider.offsetLeft;
   const walk = (x - startX) * 1.2; //scroll-fast
   slider.scrollLeft = scrollLeft - walk;
   console.log(walk);
 });
-  
--- popup.js
+++ popup.js
@@ -1,52 +1,94 @@
-function toggle(){
-    var blur = document.getElementById("blur");
-    blur.classList.toggle("active")
-    var popup = document.getElementById("popup1");
-    popup.classList.toggle("active")
-    var stopScroll = document.getElementById("body");
-    stopScroll.classList.toggle("scrollout")
-    var stopNavbar = document.getElementById("navbar");
-    stopNavbar.classList.toggle("notshow")
+function toggle() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup1");
+  popup.classList.toggle("active");
+  var stopScroll = document.getElementById("body");
+  stopScroll.classList.toggle("scrollout");
+  var stopNavbar = document.getElementById("navbar");
+  stopNavbar.classList.toggle("notshow");
+  // Set the src attribute of the video element
+  var video = document.querySelector(".vid1");
+  if (popup.classList.contains("active")) {
+    video.src = "Video/Fairsource.mp4";
+  } else {
+    video.removeAttribute("src");
+  }
 }
-function toggle2(){
-    var blur = document.getElementById("blur");
-    blur.classList.toggle("active")
-    var popup = document.getElementById("popup2");
-    popup.classList.toggle("active")
-    var stopScroll = document.getElementById("body");
-    stopScroll.classList.toggle("scrollout")
-    var stopNavbar = document.getElementById("navbar");
-    stopNavbar.classList.toggle("notshow")
+function toggle2() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup2");
+  popup.classList.toggle("active");
+  var stopScroll = document.getElementById("body");
+  stopScroll.classList.toggle("scrollout");
+  var stopNavbar = document.getElementById("navbar");
+  stopNavbar.classList.toggle("notshow");
+  var video = document.querySelector(".vid2");
+  if (popup.classList.contains("active")) {
+    video.src = "Video/ETA.mp4";
+  } else {
+    video.removeAttribute("src");
+  }
+}
 
+function toggle3() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup3");
+  popup.classList.toggle("active");
+  var stopScroll = document.getElementById("body");
+  stopScroll.classList.toggle("scrollout");
+  var stopNavbar = document.getElementById("navbar");
+  stopNavbar.classList.toggle("notshow");
+  var video = document.querySelector(".vid3");
+  if (popup.classList.contains("active")) {
+    video.src = "Video/Thesis.mp4";
+  } else {
+    video.removeAttribute("src");
+  }
 }
-
-function toggle3(){
-    var blur = document.getElementById("blur");
-    blur.classList.toggle("active")
-    var popup = document.getElementById("popup3");
-    popup.classList.toggle("active")
-    var stopScroll = document.getElementById("body");
-    stopScroll.classList.toggle("scrollout")
-    var stopNavbar = document.getElementById("navbar");
-    stopNavbar.classList.toggle("notshow")
+function toggle4() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup4");
+  popup.classList.toggle("active");
+  var stopScroll = document.getElementById("body");
+  stopScroll.classList.toggle("scrollout");
+  var stopNavbar = document.getElementById("navbar");
+  stopNavbar.classList.toggle("notshow");
+  var video = document.querySelector(".vid4");
+  if (popup.classList.contains("active")) {
+    video.src = "Video/Gruene lebensmittel.mp4";
+  } else {
+    video.removeAttribute("src");
+  }
 }
-function toggle4(){
-    var blur = document.getElementById("blur");
-    blur.classList.toggle("active")
-    var popup = document.getElementById("popup4");
-    popup.classList.toggle("active")
-    var stopScroll = document.getElementById("body");
-    stopScroll.classList.toggle("scrollout")
-    var stopNavbar = document.getElementById("navbar");
-    stopNavbar.classList.toggle("notshow")
-}
-function toggle5(){
-    var blur = document.getElementById("blur");
-    blur.classList.toggle("active")
-    var popup = document.getElementById("popup5");
-    popup.classList.toggle("active")
-    var stopScroll = document.getElementById("body");
-    stopScroll.classList.toggle("scrollout")
-    var stopNavbar = document.getElementById("navbar");
-    stopNavbar.classList.toggle("notshow")
+function toggle5() {
+  var blur = document.getElementById("blur");
+  blur.classList.toggle("active");
+  var popup = document.getElementById("popup5");
+  popup.classList.toggle("active");
+  var stopScroll = document.getElementById("body");
+  stopScroll.classList.toggle("scrollout");
+  var stopNavbar = document.getElementById("navbar");
+  stopNavbar.classList.toggle("notshow");
+  // load images when popup is opened
+  var images = document.querySelectorAll("#popup5 img[data-src]");
+  images.forEach(function (img) {
+    img.setAttribute("src", img.getAttribute("data-src"));
+    img.removeAttribute("data-src");
+  });
+  var video = document.querySelector(".vid5");
+  if (popup.classList.contains("active")) {
+    video.src = "Video/wayo_intro.mp4#t=0.001";
+  } else {
+    video.removeAttribute("src");
+  }
+  var video = document.querySelector(".vid6");
+  if (popup.classList.contains("active")) {
+    video.src = "Video/wayo_intro_ad_2.mp4#t=0.001";
+  } else {
+    video.removeAttribute("src");
+  }
 }
